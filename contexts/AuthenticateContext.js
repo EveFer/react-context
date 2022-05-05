cb4d064c5701df4f42737b52502e1e8222e7b7a5
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-05-05 00:26:47 -0500

    [UPDATE] use context

--- contexts/AuthenticateContext.js
+++ contexts/AuthenticateContext.js
@@ -5,9 +5,9 @@ const AuthenticateContext = createContext()
 // provider - Component que wrapeara a los componentes que tendra
 
  const AuthenticateProvider = ({children}) => {
-    const [userAuth, setUserAuth] = useState({})
-
-    const data = {userAuth, setUserAuth}
+    // const [userAuth, setUserAuth] = useState({})
+    const [responseForm, setResponseForm] = useState({})
+    const data = {responseForm, serResponseForm}
     return (
         <AuthenticateContext.Provider value={data}>
             {children}
